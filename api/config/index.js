72351f4366f2bc7043e3ebf6e013f1a536d25774
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2025-02-24 14:47:42 +0300

    feat: Enable file system-based dynamic route registration #1

--- api/config/index.js
+++ api/config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    "PORT": process.env.PORT || "3000",
     "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb://127.0.0.1:27017/administration_system",
     "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
 };
