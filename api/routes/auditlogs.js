1ea332fcb0d4114a9b7c7ad773d57ddd41e575d9
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2025-03-10 11:41:47 +0300

    feat: Initialized user audit logs infrastructure #4

---
+++ api/routes/auditlogs.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const moment = require("moment");
+const AuditLogs = require("../db/models/AudiLogs");
+const Response = require("../lib/Response");
+
+
+router.post ("/", async (req, res) => {
+    
+    try {
+
+        let body = req.body
+        let query = {}
+
+        let limit = body.limit;
+        let skip = body.skip;
+
+        if (typeof skip !== "numeric") {
+            skip = 0;
+        }
+
+        if (typeof limit !== "numeric" || limit > 500) {
+            limit = 500;
+        }
+
+        if (body.begin_date && body.end_date) {
+            query.created_at = {
+                $gte: moment(body.begin_date),
+                $lte: moment(body.end_date)
+            }
+        } else {
+            query.created_at = {
+                $gte: moment().subtract(1, "day").startOf("day"),
+                $lte: moment()
+            }
+        }
+
+
+        let auditLogs = await AuditLogs.find( query ).sort({ created_by: -1 }).limit( limit ).skip( skip );
+
+        res.json(Response.successResponse(auditLogs));
+
+    } catch (err) {
+        let errorResponse = Response.errorResponse(err);
+        res.status(errorResponse.code).json(errorResponse);
+    }
+})
+
+module.exports = router;
