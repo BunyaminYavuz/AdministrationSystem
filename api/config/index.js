0a9ad2f926b6fc327453443fe2401ba4a54902b2
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2025-03-15 00:27:52 +0300

    feat: Integrate Winston for audit logging
    This commit adds Winston logging to the project for audit log purposes.
    - Installed the Winston library.
    - Added logging configuration to \lib/logger/logger.js\.
    - Updated the \LoggerClass\ class. #4

--- api/config/index.js
+++ api/config/index.js
@@ -2,4 +2,5 @@ module.exports = {
     "PORT": process.env.PORT || "3000",
     "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb://127.0.0.1:27017/administration_system",
     "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
+    "LOG_FILE_PATH": process.env.LOG_FILE_PATH || "./logs/app.log"
 };
